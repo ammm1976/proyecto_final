c3c5176856053e9af6c08704f710f275e57a24d7
ammm1976 <157803318+ammm1976@users.noreply.github.com>
2024-03-15 13:43:58 +0100

    data y bucle

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import './style.css'
+import discos from './data/discos'
+
+const favoritosSection = document.querySelector("#discosFavoritos")
+const otrosSection = document.querySelector("#otrosDiscos");
+
+for (const disco of discos) {
+  const article = document.createElement("article");
+  article.innerHTML = `
+    <img src=${disco.portada} alt=${disco.titulo}
+    <h3>${disco.titulo}</h3>
+    <h3>${disco.año}</h3>
+  `;
+  if (disco.favorito){
+    favoritosSection.appendChild(article);
+  } else {
+    otrosSection.appendChild(article)
+ }    
+}
