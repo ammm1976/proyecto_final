fbb16c1fbfab1479739529ea99f9028914c8fd4f
ammm1976 <157803318+ammm1976@users.noreply.github.com>
2024-03-18 13:40:42 +0100

    spa y mas diseño

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
-import './style.css'
-import discos from './data/discos'
+import "./style.css";
+import discos from "./data/discos";
 
 const temaBtn = document.querySelector("#tema");
 temaBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
 
-const favoritosSection = document.querySelector("#discosFavoritos")
-const otrosSection = document.querySelector("#otrosDiscos");
+const badReligionLink = document.querySelector("#badReligionLink");
+const lagwagonLink = document.querySelector("#lagwagonLink");
+const nofxLink = document.querySelector("#nofxLink");
 
-for (const disco of discos) {
-  const article = document.createElement("article");
-  article.innerHTML = `
-    <img src=${disco.portada} alt=${disco.titulo}
-    <h3>${disco.titulo}</h3>
-    <h3>${disco.año}</h3>
-  `;
-  if (disco.favorito) {
-    favoritosSection.appendChild(article);
-  } else {
-    otrosSection.appendChild(article)
- }    
-}
+const PintarGrupo = (grupo) => {
+  const favoritosSection = document.querySelector("#discosFavoritos");
+  favoritosSection.innerHTML = "";
+  const otrosSection = document.querySelector("#otrosDiscos");
+  otrosSection.innerHTML = "";
+  
+  for (const disco of discos) {
+    if (disco.grupo == grupo) {
+      const article = document.createElement("article");
+      article.innerHTML = `
+        <img src=${disco.portada} alt=${disco.titulo}
+        <h3>${disco.titulo}</h3>
+        <h3>${disco.año}</h3>
+      `;
+      if (disco.favorito) {
+        favoritosSection.appendChild(article);
+      } else {
+        otrosSection.appendChild(article);
+      }
+    }
+  }
+};
+
+badReligionLink.addEventListener("click", () => {
+  PintarGrupo("Bad Religion");
+});
+
+lagwagonLink.addEventListener("click", () => {
+  PintarGrupo("Lagwagon");
+});
+nofxLink.addEventListener("click", () => {
+  PintarGrupo("NoFX");
+});
+
+PintarGrupo("Bad Religion");
